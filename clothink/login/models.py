c3162662196a53d9d21bffde0ed5b22299acc951
gerryc-0 <gerardh.carrigan@gmail.com>
2021-03-08 22:42:22 +0000

    registration feature created

--- clothink/login/models.py
+++ clothink/login/models.py
@@ -3,7 +3,14 @@ from django.db import models
 # Create your models here.
 class Login(models.Model):
 	name = models.CharField(max_length=300)
-	password = models.TextField() 
+	password = models.TextField()
+
+	def __str__(self):
+		return self.name
+
+class Registration(models.Model):
+	name = models.CharField(max_length=300)
+	password = models.TextField()
 
 	def __str__(self):
 		return self.name
--- clothink/login/views.py
+++ clothink/login/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
-from login.models import Login
+from login.models import Login, Registration
 # Create your views here.
 class LoginView(generic.ListView):
 	model = Login
 	template_name = 'login/user_login.html'
+
+class RegisterView(generic.ListView):
+	model = Registration
+	template_name = 'login/registration.html'
