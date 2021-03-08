7cb2053cc17996dae514b5dc6a4f86653fae2282
Daniel Gallagher <daniel.gallagher1@ucdconnect.ie>
2021-03-08 20:15:37 +0000

    login model created

--- clothink/login/models.py
+++ clothink/login/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Login(models.Model):
 	name = models.CharField(max_length=300)
-	password = models.TextField()
+	password = models.TextField() 
 
 	def __str__(self):
 		return self.name
