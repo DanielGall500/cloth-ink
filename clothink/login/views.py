0f0046b52f2a8a9575d88fa79ff644b7a5c77688
Daniel Gallagher <daniel.gallagher1@ucdconnect.ie>
2021-03-15 20:12:04 +0000

    redirect added

--- clothink/login/views.py
+++ clothink/login/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
-from login.models import Login, Registration
+from django.urls import reverse, reverse_lazy
+from login.models import Login, Registration, User
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
-class RegisterView(generic.ListView):
+class RegisterView(generic.CreateView):
 	model = Registration
 	form_class = UserCreationForm
-	#context = {'form': form}
 	template_name = 'login/registration.html'
+	success_url = reverse_lazy('service:home')
 
 
-class LoginView(generic.ListView):
+class LoginView(generic.CreateView):
 	model = Login
 	template_name = 'login/user_login.html'
