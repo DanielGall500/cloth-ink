ee33876d77d16a208c06f7280c013ea945664ae5
gerryc-0 <gerardh.carrigan@gmail.com>
2021-06-03 14:37:22 +0100

    logintutorial cleanup

--- clothink/login/views.py
+++ clothink/login/views.py
@@ -1,9 +1,11 @@
+from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.views import generic
 from django.urls import reverse, reverse_lazy
 from login.models import Login, Registration
 from django.contrib.auth import logout, login
 from django.contrib.auth.forms import UserCreationForm, authenticate
+
 
 class RegisterView(generic.CreateView):
 	model = Registration
@@ -16,16 +18,15 @@ class LoginView(generic.CreateView):
 	model = Login
 	template_name = 'login/user_login.html'
 	success_url = reverse_lazy('service:home')
-	fields = ("name","password")
+	fields = ("name", "password")
+
 
 def login_auth(request):
-    name = request.POST.get('name')
-    password = request.POST.get('password')
-
-    user = authenticate(request, username=name, password=password)
-
-    if user is not None:
-	    login(request, user)
-	    return redirect('service:home')
-    else:
-        return redirect('login:user_registration')
+	name = request.POST.get('name')
+	password = request.POST.get('password')
+	user = authenticate(request, username=name, password=password)
+	if user is not None:
+		login(request, user)
+		return redirect('service:home')
+	else:
+		return redirect('login:user_registration')
