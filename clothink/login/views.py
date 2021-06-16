b5b9049729a77d04415fa77767d602036fecd787
Daniel Gallagher <daniel.gallagher1@ucdconnect.ie>
2021-06-16 15:25:17 +0200

    user auth improved

--- clothink/login/views.py
+++ clothink/login/views.py
@@ -3,41 +3,43 @@ from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.views import generic
 from django.urls import reverse, reverse_lazy
-from login.models import Login, Registration
 from django.contrib.auth import logout, login
 from django.contrib.auth.forms import UserCreationForm, authenticate
+from userprofile.models import User
+from userprofile.admin import UserCreationForm, UserLoginForm
 
 
 class RegisterView(generic.CreateView):
-
-	model = Registration
+	model = User
 	form_class = UserCreationForm
 	template_name = 'login/registration.html'
 	success_url = reverse_lazy('service:home')
 
 
 class LoginView(generic.CreateView):
-
-	model = Login
+	model = User
+	form_class = UserLoginForm
 	template_name = 'login/user_login.html'
 	success_url = reverse_lazy('service:home')
-	fields = ("name", "password")
 
 
 def logout_view(request):
-
     logout(request)
     messages.info(request, "You have successfully logged out.")
     return redirect("service:home")
 
 
 def login_auth(request):
+	if request.method == 'POST':
+		email = request.POST.get('email','')
+		password = request.POST.get('password','')
+		
+		user = authenticate(request, email=email, password=password)
 
-	name = request.POST.get('name')
-	password = request.POST.get('password')
-	user = authenticate(request, username=name, password=password)
-	if user is not None:
-		login(request, user)
-		return redirect('service:home')
+		if user is not None:
+			login(request, user)
+			return redirect('service:home')
+		else:
+			return redirect('login:user_registration')
 	else:
 		return redirect('login:user_registration')
