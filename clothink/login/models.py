44c1a0813e16033425e003d4a55b22e58a8a0232
Daniel Gallagher <daniel.gallagher1@ucdconnect.ie>
2021-06-16 15:25:29 +0200

    removed unnecessary models

--- clothink/login/models.py
+++ clothink/login/models.py
@@ -1,17 +1,2 @@
 from django.db import models
 from django.urls import reverse
-
-# Create your models here.
-class Login(models.Model):
-	name = models.CharField(max_length=300)
-	password = models.TextField()
-
-	def __str__(self):
-		return self.name
-
-class Registration(models.Model):
-	name = models.CharField(max_length=300)
-	password = models.TextField()
-
-	def __str__(self):
-		return self.name
